3bb0f524599d7a6ed276b0858aca8fddf2999ee2
eliasv <elias.beauchamp@upr.edu>
2014-11-05 19:32:34 -0400

    constructor added.

---
+++ BinaryHeap/BSTNode.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryHeap
+{
+    public class BTNode<T>
+    {
+        private T data;
+        private BTNode<T> parent;
+        private BTNode<T> childLeft;
+        private BTNode<T> childRight;
+
+        public void BTNode<T>()
+        {
+            parent = null;
+            childLeft = null;
+            childRight = null;
+        }
+    }
+}
